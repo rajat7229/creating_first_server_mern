07a534880bf7ee6c20ae8eba4d6e13f3cdd83847
Rajat <rajat7229@gmai.com>
2022-08-20 22:00:00 +0530

    code for first nodeJs server

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const port = 7229;
+
+http.createServer((request, response) => {
+    response.writeHead(200, {'Content-Type': "text/html"});
+    response.write("<h1>Hello, this is from my server</h1>");
+    response.end();
+}).listen(port, () =>{
+    console.log(`Nodejs server started on port ${port}`);
+});
+
+//  http://localhost:7229 // use this to open server
